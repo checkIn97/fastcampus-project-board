506c9524261b1592ed977dbf7b41213ed1661824
JeongSan <vnfmstks1733@naver.com>
2024-08-06 18:10:12 +0900

    #16 - 게시판, 게시글 페이지에 부트스트랩 및 예제 코드 적용 * https://getbootstrap.com/docs/5.2/getting-started/introduction/ * https://bootdey.com/snippets/view/General-Search-Results

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -3,16 +3,17 @@ package com.fastcampus.projectboard.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin().and()
-                .build();
+            http
+                    .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).formLogin();
+            return http.build();
     }
 }
